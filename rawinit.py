eef89fc860c163cef7a5d5eff1e41825ce742f8a
Lorenzo Gaggini <lg@lgaggini.net>
2018-08-22 15:14:02 +0200

    rawinit refactor by pysshops

--- rawinit.py
+++ rawinit.py
@@ -11,6 +11,7 @@ import coloredlogs
 import subprocess
 import os
 import importlib
+from pysshops import SshOps, SftpOps, SshCommandBlockingException
 
 
 SETTINGS_KEY = ['PROXMOX', 'SSH_HOST_KEY', 'DEV', 'TMP_DIR', 'STATIC_DIR',
@@ -70,60 +71,23 @@ def template_compile(configs):
                 config_file.write(j2_template.render(var=var))
 
 
-def get_proxmox_ssh(proxmox):
-    """ get a ssh connection to proxmox """
-    logger.info('opening connection to %s' % proxmox['SSH_HOST'])
-    proxmox_ssh = SSHClient()
-    proxmox_ssh.set_missing_host_key_policy(WarningPolicy())
-    proxmox_ssh.connect(proxmox['SSH_HOST'],
-                        username=proxmox['USER'].split('@')[0])
-    return proxmox_ssh
-
-
-def remote_command(ssh, command):
-    """ execute a remote command by the ssh connection """
-    stdin, stdout, stderr = ssh.exec_command(command)
-    stdout_str = ' ,'.join(stdout.readlines())
-    stderr_str = ' ,'.join(stderr.readlines())
-    logger.debug('stdout: ' + stdout_str)
-    logger.debug('stderr: ' + stderr_str)
-    return stdout.channel.recv_exit_status(), stdout_str, stderr_str
-
-
-def check_exit(exit, stdout, stderr, block=True):
-    """ check the exit code and if not 0 log stderror and exit
-    (if blocking command) """
-    if exit == 0:
-        return
-    else:
-        logger.error(stderr)
-        if block:
-            sys.exit(127)
-
-
 def nbd_module(ssh):
     """ load the nbd kernel module """
     logger.info('modprobe of nbd module')
     command = 'sudo modprobe nbd'
-    return remote_command(ssh, command)
+    ssh.remote_command(command)
 
 
 def image_mount(ssh, dev, src, dst, part):
     """ create a custom mountpoint and mount a qemu
     supported image by nbd """
     command = 'sudo qemu-nbd -f raw -c %s %s' % (dev, src)
-    exit, stdout, stderr = remote_command(ssh, command)
-    if (exit == 0):
-        command = 'mkdir -p %s' % dst
-        exit, stdout, stderr = remote_command(ssh, command)
-        if (exit == 0):
-            src_nbd = '%sp%s' % (dev, part)
-            command = 'sudo mount %s %s' % (src_nbd, dst)
-            return remote_command(ssh, command)
-        else:
-            return 127, stdout, stderr
-    else:
-        return 127, stdout, stderr
+    ssh.remote_command(command)
+    command = 'mkdir -p %s' % dst
+    ssh.remote_command(command)
+    src_nbd = '%sp%s' % (dev, part)
+    command = 'sudo mount %s %s' % (src_nbd, dst)
+    ssh.remote_command(command)
 
 
 def image_umount(ssh, dev, src, dst):
@@ -131,68 +95,28 @@ def image_umount(ssh, dev, src, dst):
     and remove custom mountpoint """
     double_check_path(dst, cfg['WORKING_MNT'])
     command = 'sudo umount %s' % dst
-    exit, stdout, stderr = remote_command(ssh, command)
-    if (exit == 0):
-        command = 'rmdir %s' % dst
-        exit, stdout, stderr = remote_command(ssh, command)
-        if (exit == 0):
-            command = 'sudo qemu-nbd -d %s' % (dev)
-            return remote_command(ssh, command)
-        else:
-            return 127, stdout, stderr
-    else:
-        return 127, stdout, stderr
+    ssh.remote_command(command)
+    command = 'rmdir %s' % dst
+    ssh.remote_command(command)
+    command = 'sudo qemu-nbd -d %s' % (dev)
+    ssh.remote_command(command)
 
 
 def check_mount(ssh, dev, src, dst):
     """ check if the target mount is mounted """
     command = 'mount | grep -q %s' % dst
-    exit, stdout, stderr = remote_command(ssh, command)
-    if exit == 0:
-        logger.debug('%s is just mounted' % dst)
-        return True
-    else:
-        logger.debug('%s is not mounted' % dst)
+    try:
+        ssh.remote_command(command)
+    except SshCommandBlockingException:
         return False
+    return True
 
 
 def ssh_folder_init(ssh, dst):
     """ create and set permission for the user .ssh folder """
-    cmd = 'mkdir -p %s/root/.ssh && chmod 0700 %s/root/.ssh' % (dst,
-                                                                dst)
-    exit, stdout, stderr = remote_command(ssh, cmd)
-
-
-def deploy(ssh, src, dst, priv_key=False, pub_key=False):
-    """ deploys the src file on dst wit some special managemente for
-    ssh keys """
-    if not os.path.exists(src):
-        logger.warning('File %s not compiled and will not be deployed' % src)
-        return
-
-    logger.info('deploy %s to %s' % (src, dst))
-    if(not double_check_path(dst, cfg['WORKING_MNT'])):
-        return
-    try:
-        proxmox_sftp = ssh.open_sftp()
-    except Exception as ex:
-        logger.error('SFTP exception: ' + str(ex))
-        sys.exit('exiting')
-
-    try:
-        proxmox_sftp.put(src, dst)
-        if priv_key:
-            proxmox_sftp.chmod(dst, 0600)
-        elif pub_key:
-            proxmox_sftp.chmod(dst, 0644)
-
-    except Exception, ex:
-        logger.error('SFTP exception: %s' % str(ex))
-
-    proxmox_sftp.close()
-    logger.info('verify the deployed file')
-    command = 'sudo ls -l %s' % (dst)
-    check_exit(*remote_command(ssh, command), block=False)
+    command = 'mkdir -p %s/root/.ssh && chmod 0700 %s/root/.ssh' % (dst,
+                                                                    dst)
+    ssh.remote_command(command)
 
 
 def generate_ssh_hostkeys(filename):
@@ -207,8 +131,7 @@ def generate_ssh_hostkeys(filename):
 def double_check_hostname(ssh, dst, expected):
     """ check if the hostname of the mounted images is consistent """
     command = 'cat %s/etc/hostname' % dst
-    exit, hostname, stderr = remote_command(ssh, command)
-    check_exit(exit, hostname, stderr)
+    hostname = ssh.remote_command(command)
     if hostname.strip() != expected:
         logger.error('the mount image on %s doesnt have the expected hostname'
                      % dst)
@@ -248,66 +171,81 @@ def rawinit(settings, configs, src, dst, dev='/dev/nbd0', part='1',
         sys.exit('exiting')
 
     # proxmox ssh connection
-    try:
-        proxmox_ssh = get_proxmox_ssh(cfg['PROXMOX'])
-    except Exception, ex:
-        logger.error('SSH exception: ' + str(ex))
-        sys.exit('exiting')
+    proxmox_srv = SshOps(cfg['PROXMOX']['SSH_HOST'],
+                         cfg['PROXMOX']['USER'].split('@')[0])
 
-    # load nbd moudule on remote host
-    logger.info('modprobing of nbd module')
-    check_exit(*nbd_module(proxmox_ssh))
-    logger.info("nbd module modprobed")
+    with proxmox_srv as proxmox_ssh:
 
-    # mount vm image
-    if check_mount(proxmox_ssh, dev, src, dst):
-        logger.info('mountpoint %s busy, unmounting it' % dst)
+        # load nbd moudule on remote host
+        logger.info('modprobing of nbd module')
+        nbd_module(proxmox_ssh)
+        logger.info("nbd module modprobed")
+
+        # mount vm image
+        if check_mount(proxmox_ssh, dev, src, dst):
+            logger.info('mountpoint %s busy, unmounting it' % dst)
+            image_umount(proxmox_ssh, dev, src, dst)
+        logger.info('mounting %s to %s by %s' % (src, dst, dev))
+        image_mount(proxmox_ssh, dev, src, dst, part)
+        logger.info('image %s mounted to %s by %sp%s' % (src, dst, dev, part))
+
+        # double check hostname on the mounted vm
+        if (not double_check_hostname(proxmox_ssh, dst, configs['template'])):
+            image_umount(proxmox_ssh, dev, src, dst)
+            sys.exit('exiting')
+
+        # deploy configurations
+        logger.info('deploy configurations')
+        custom_tmp_fd = '%s/%s' % (cfg['TMP_DIR'], configs['name'])
+
+        proxmox_sftp_srv = SftpOps(cfg['PROXMOX']['SSH_HOST'],
+                                   cfg['PROXMOX']['USER'].split('@')[0])
+        with proxmox_sftp_srv as proxmox_sftp:
+            logger.debug(proxmox_sftp)
+            logger.info('interfaces')
+            proxmox_sftp.deploy('%s/interfaces' % custom_tmp_fd,
+                                '%s/etc/network/interfaces' % dst)
+            logger.info('hostname')
+            proxmox_sftp.deploy('%s/hostname' % custom_tmp_fd,
+                                '%s/etc/hostname' % dst)
+            logger.info('serverfarm')
+            proxmox_sftp.deploy('%s/serverfarm' % custom_tmp_fd,
+                                '%s/etc/serverfarm' % dst)
+            logger.info('interfaces')
+            proxmox_sftp.deploy('%s/pu_puppetenvironment' % custom_tmp_fd,
+                                '%s/etc/pu_puppetenvironment' % dst)
+            logger.info('hosts')
+            proxmox_sftp.deploy('%s/hosts' % custom_tmp_fd,
+                                '%s/etc/hosts' % dst)
+            logger.info('puppet')
+            proxmox_sftp.deploy('%s/puppet.conf' % custom_tmp_fd,
+                                '%s/etc/puppet/puppet.conf' % dst)
+            # generate ssh host keys
+            logger.info('generate and deploy tmp RSA 2048 bit host keys')
+            generate_ssh_hostkeys('%s/%s' % (custom_tmp_fd,
+                                             cfg['SSH_HOST_KEY']))
+            logger.info('generated tmp RSA 2048 bit host keys for first ssh access')
+            priv = '%s' % cfg['SSH_HOST_KEY']
+            pub = '%s.pub' % cfg['SSH_HOST_KEY']
+            proxmox_sftp.deploy('%s/%s' % (custom_tmp_fd, priv),
+                                '%s/etc/ssh/%s' % (dst, priv))
+            proxmox_sftp.chmod('%s/etc/ssh/%s' % (dst, priv), 0600)
+            proxmox_sftp.deploy('%s/%s' % (custom_tmp_fd, pub), '%s/etc/ssh/%s'
+                                % (dst, pub))
+            proxmox_sftp.chmod('%s/etc/ssh/%s' % (dst, pub), 0644)
+            # public key to ssh access as root user
+            logger.info('creating the root .ssh folder')
+            ssh_folder_init(proxmox_ssh, dst)
+            proxmox_sftp.deploy('%s/authorized_keys' % cfg['STATIC_DIR'],
+                                '%s/root/.ssh/authorized_keys' % dst)
+            proxmox_sftp.chmod('%s/root/.ssh/authorized_keys' % dst, 0600)
+            logger.info('config deployed')
+
+        # deploy end, umount vm disk and close ssh connection
+        logger.info('unmounting of %s to %s by %s' % (src, dst, dev))
         image_umount(proxmox_ssh, dev, src, dst)
-    logger.info('mounting %s to %s by %s' % (src, dst, dev))
-    check_exit(*image_mount(proxmox_ssh, dev, src, dst, part))
-    logger.info('image %s mounted to %s by %sp%s' % (src, dst, dev, part))
-
-    # double check hostname on the mounted vm
-    if (not double_check_hostname(proxmox_ssh, dst, configs['template'])):
-        check_exit(*image_umount(proxmox_ssh, dev, src, dst))
-        sys.exit('exiting')
-
-    # deploy configurations
-    logger.info('deploy configurations')
-    custom_tmp_fd = '%s/%s' % (cfg['TMP_DIR'], configs['name'])
-    deploy(proxmox_ssh, '%s/interfaces' % custom_tmp_fd,
-           '%s/etc/network/interfaces' % dst)
-    deploy(proxmox_ssh, '%s/hostname' % custom_tmp_fd, '%s/etc/hostname' % dst)
-    deploy(proxmox_ssh, '%s/serverfarm' % custom_tmp_fd,
-           '%s/etc/serverfarm' % dst)
-    deploy(proxmox_ssh, '%s/pu_puppetenvironment' % custom_tmp_fd,
-           '%s/etc/pu_puppetenvironment' % dst)
-    deploy(proxmox_ssh, '%s/hosts' % custom_tmp_fd, '%s/etc/hosts' % dst)
-    deploy(proxmox_ssh, '%s/puppet.conf' % custom_tmp_fd,
-           '%s/etc/puppet/puppet.conf' % dst)
-    # generate ssh host keys
-    logger.info('generate and deploy tmp RSA 2048 bit host keys')
-    generate_ssh_hostkeys('%s/%s' % (custom_tmp_fd, cfg['SSH_HOST_KEY']))
-    logger.info('generated tmp RSA 2048 bit host keys for first ssh access')
-    priv = '%s' % cfg['SSH_HOST_KEY']
-    pub = '%s.pub' % cfg['SSH_HOST_KEY']
-    deploy(proxmox_ssh, '%s/%s' % (custom_tmp_fd, priv), '%s/etc/ssh/%s'
-           % (dst, priv), priv_key=True)
-    deploy(proxmox_ssh, '%s/%s' % (custom_tmp_fd, pub), '%s/etc/ssh/%s'
-           % (dst, pub), pub_key=True)
-    # public key to ssh access as root user
-    logger.info('creating the root .ssh folder')
-    ssh_folder_init(proxmox_ssh, dst)
-    deploy(proxmox_ssh, '%s/authorized_keys' % cfg['STATIC_DIR'],
-           '%s/root/.ssh/authorized_keys' % dst, priv_key=True)
-    logger.info('config deployed')
-
-    # deploy end, umount vm disk and close ssh connection
-    logger.info('unmounting of %s to %s by %s' % (src, dst, dev))
-    check_exit(*image_umount(proxmox_ssh, dev, src, dst))
-    logger.info('image %s unmounted from %s by %s' % (src, dst, dev))
-    logger.info('closing connection to %s' % cfg['PROXMOX']['SSH_HOST'])
-    proxmox_ssh.close()
+        logger.info('image %s unmounted from %s by %s' % (src, dst, dev))
+        logger.info('closing connection to %s' % cfg['PROXMOX']['SSH_HOST'])
     logger.info('connection to %s closed' % cfg['PROXMOX']['SSH_HOST'])
 
 
